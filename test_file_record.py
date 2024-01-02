cb928a37b068a7fb227a34accbaa989d015312b4
f3rn0s <finn@volkis.com.au>
2024-01-02 10:18:11 +1000

    fix typo

--- test_file_record.py
+++ test_file_record.py
@@ -2,13 +2,9 @@ from adiskreader.filesystems.ntfs.structures.filerecord import FileRecord
 import asyncio
 
 async def amain():
-    with open('download.dat', 'rb') as x:
+    with open('resident_data_stream', 'rb') as x:
         fr = FileRecord.from_bytes(x.read(), None, None)
         print(fr)
-
-
-
-
 
 def main():
     asyncio.run(amain())
